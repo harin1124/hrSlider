ee313e35bc0f663eb2bfb57b7e95ab45e438673f
harin1124 <lolrin1124@gmail.com>
2020-02-05 18:12:47 +0900

    .

--- js/hrSlider.js
+++ js/hrSlider.js
@@ -1,12 +1,22 @@
 $.fn.hrSlider=function(option){
-	console.log(option);
+	var obj={container:this};
+	console.log('option value >> ',option);
 
 	event={};
 	event.auto=function(){
-		console.log('auto function')
+		console.log('auto function');
+
+		debugger;
+		//슬라이드 사이즈 구하기
+	}
+
+	set={};
+	set.init=function(){
+		console.log('init function');
 	}
 
 	// TYPE | auto, static
+	set.init();
 	if(option.type!=undefined || option.type!=null){
 		if(option.type=='auto'){
 			//debugger;
